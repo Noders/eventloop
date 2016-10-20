3d47932de7a9501bacf240fe6629dbe83ea8d5d2
fetorres <fetorres@groupon.com>
2016-10-20 20:11:29 -0300

    update

--- src/components/Sponsors/index.js
+++ src/components/Sponsors/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import microsoft from '../../images/microsoft.png';
 import uss from '../../images/uss.png';
 import pageload from '../../images/pageload.png';
+import mediastream from '../../images/mediastream.png';
 
 // Partners
 import noders from '../../images/noders.png';
@@ -24,18 +25,33 @@ export default class Sponsors extends Component {
           <h2>Nuestros Sponsors</h2>
         </header>
         <div className={style.body}>
-          <img src={microsoft} alt="microsoft" className={style.images} />
-          <img src={uss} alt="universidad san sebastian" className={style.images} />
-          <img src={pageload} alt="pageload" className={style.images} />
+          <a className={style.imageLinks} href="http://www.microsoft.com">
+            <img src={microsoft} alt="microsoft" className={style.images} />
+          </a>
+          <a className={style.imageLinks} href="http://www.uss.cl">
+            <img src={uss} alt="universidad san sebastian" className={style.images} />
+          </a>
+          <a className={style.imageLinks} href="http://www.pageload.io">
+            <img src={pageload} alt="pageload" className={style.images} />
+          </a>
+          <a className={style.imageLinks} href="http://www.mediastre.am">
+            <img src={mediastream} alt="mediastream" className={style.images} />
+          </a>
         </div>
         <br />
         <header className={['major', style.subTitle].join(' ')}>
           <h2 className={style.subTitle}>Community Partners</h2>
         </header>
         <div className={style.body}>
-          <img src={noders} alt="noders" className={style.imagesSponsors} />
-          <img src={proin} alt="proin" className={style.imagesSponsors} />
-          <img src={ivdevs} alt="iv devs" className={style.imagesSponsors} />
+          <a className={style.imageLinks} href="http://www.noders.com">
+            <img src={noders} alt="noders" className={style.imagesSponsors} />
+          </a>
+          <a className={style.imageLinks} href="http://www.proin.cl">
+            <img src={proin} alt="proin" className={style.imagesSponsors} />
+          </a>
+          <a className={style.imageLinks} href="http://www.ivdevs.cl">
+            <img src={ivdevs} alt="iv devs" className={style.imagesSponsors} />
+          </a>
         </div>
         <footer className="major">
           <p>
