68525b68c1758cf100f7324cfc7a40594f7e1fc5
felipe torres <felipe.torressepulveda@gmail.com>
2016-10-23 01:43:10 -0300

    Adding webpack dev server

---
+++ webpack/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+
+import webpackDevConfig from './webpack.config';
+import webpackProdConfig from './webpack.dev.config';
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const webpackConfig = isDeveloping ? webpackDevConfig : webpackProdConfig;
+
+const port = isDeveloping ? 3010 : process.env.PORT || 3010;
+const app = express();
+
+if (isDeveloping) {
+  const compiler = webpack(webpackConfig);
+  const middleware = webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.get('*', (req, res) => {
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    res.end();
+  });
+} else {
+  app.use(express.static(path.resolve(__dirname, '..', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  });
+}
+
+
+app.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,129 @@
+/*
+  eslint
+    no-var: 0,
+    import/no-extraneous-dependencies: 0
+*/
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var cssnext = require('postcss-cssnext');
+var nested = require('postcss-nested');
+var postcssAssets = require('postcss-assets');
+
+// var colors = require('colors');
+
+var postCSSConfig = () => {
+  return [nested, cssnext(), postcssAssets()];
+};
+
+module.exports = {
+  devtool: 'cheap-source-map',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      // filename: path.resolve(__dirname, '..', 'src', 'build', 'index.html'),
+      template: path.resolve(__dirname, '..', 'src', 'template', 'index.hbs')
+    }),
+    new ExtractTextPlugin({
+      filename: 'styles.css', allChunks: true
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: process.env.NODE_ENV === 'production',
+      options: {
+        context: path.join(__dirname, '..', 'src'),
+        output: {
+          path: path.join(__dirname, '..', 'build')
+        },
+        babel: {
+          presets: [
+            'es2015', 'stage-0'
+          ],
+          plugins: ['transform-runtime']
+        },
+        postcss: postCSSConfig
+      }
+    }),
+    new FaviconsWebpackPlugin({
+      // Your source logo
+      logo: './src/images/eventloop.svg',
+      // The prefix for all image files (might be a folder or a name)
+      prefix: 'icons-[hash]/',
+      // Emit all stats of the generated icons
+      emitStats: false,
+      // The name of the json containing all favicon information
+      statsFilename: 'iconstats-[hash].json',
+      // Generate a cache file with control hashes and
+      // don't rebuild the favicons until those hashes change
+      persistentCache: true,
+      // Inject the html into the html-webpack-plugin
+      inject: true,
+      // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+      background: '#fff',
+      // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: true,
+        favicons: true,
+        firefox: true,
+        opengraph: true,
+        twitter: true,
+        yandex: true,
+        windows: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development'),
+        BABEL_ENV: JSON.stringify('development')
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery'
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        include: [path.resolve(__dirname, '..', 'src/components')],
+        loader: ['style?sourceMap', 'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]']
+      }, {
+        test: /\.css$/,
+        include: [path.resolve(__dirname, '..', 'src/css')],
+        loader: ['style', 'css']
+      }, {
+        test: /\.(ttf|eot|svg|ttf|otf|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file-loader'
+      }, {
+        test: /\.(jpg|png|gif)$/,
+        loaders: ['file-loader', 'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}']
+      }, {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react', 'react-hmre']
+        }
+      }, {
+        test: /\.(handlebars|hbs)$/,
+        loader: 'handlebars-loader',
+        query: {
+          partialDirs: [path.join(__dirname, '..', 'src', 'template')]
+        }
+      }
+    ]
+  }
+};
